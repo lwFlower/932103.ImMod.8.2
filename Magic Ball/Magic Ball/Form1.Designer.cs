c75628acaa204938be4ffcf98f80a5bda1b51653
Arina <144441111+lwFlower@users.noreply.github.com>
2024-05-02 16:57:30 +0700

    Add files via upload

---
+++ Magic Ball/Magic Ball/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Magic_Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Answer = new System.Windows.Forms.Label();
+            this.Question = new System.Windows.Forms.TextBox();
+            this.MagicPic = new System.Windows.Forms.PictureBox();
+            this.labelP = new System.Windows.Forms.Label();
+            this.labelK = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MagicPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Answer
+            // 
+            this.Answer.AutoSize = true;
+            this.Answer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(35)))), ((int)(((byte)(125)))));
+            this.Answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Answer.ForeColor = System.Drawing.Color.White;
+            this.Answer.Location = new System.Drawing.Point(224, 197);
+            this.Answer.MaximumSize = new System.Drawing.Size(140, 140);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(126, 75);
+            this.Answer.TabIndex = 2;
+            this.Answer.Text = "ффффффффффффффффф";
+            this.Answer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Answer.Visible = false;
+            // 
+            // Question
+            // 
+            this.Question.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Question.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Question.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Question.Location = new System.Drawing.Point(0, 0);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(572, 26);
+            this.Question.TabIndex = 1;
+            this.Question.Text = "Задайте свой вопрос и потрясите шар";
+            this.Question.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Question.Click += new System.EventHandler(this.Question_Click);
+            this.Question.TextChanged += new System.EventHandler(this.Question_TextChanged);
+            // 
+            // MagicPic
+            // 
+            this.MagicPic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MagicPic.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.MagicPic.Image = global::Magic_Ball.Properties.Resources._8_Ball;
+            this.MagicPic.Location = new System.Drawing.Point(0, 28);
+            this.MagicPic.Name = "MagicPic";
+            this.MagicPic.Size = new System.Drawing.Size(572, 455);
+            this.MagicPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MagicPic.TabIndex = 0;
+            this.MagicPic.TabStop = false;
+            this.MagicPic.Click += new System.EventHandler(this.MagicPic_Click);
+            // 
+            // labelP
+            // 
+            this.labelP.AutoSize = true;
+            this.labelP.Location = new System.Drawing.Point(25, 407);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(28, 16);
+            this.labelP.TabIndex = 3;
+            this.labelP.Text = "p = ";
+            // 
+            // labelK
+            // 
+            this.labelK.AutoSize = true;
+            this.labelK.Location = new System.Drawing.Point(25, 441);
+            this.labelK.Name = "labelK";
+            this.labelK.Size = new System.Drawing.Size(27, 16);
+            this.labelK.TabIndex = 4;
+            this.labelK.Text = "k = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 483);
+            this.Controls.Add(this.labelK);
+            this.Controls.Add(this.labelP);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.Question);
+            this.Controls.Add(this.MagicPic);
+            this.Name = "Form1";
+            this.Text = "Magic 8 Ball";
+            ((System.ComponentModel.ISupportInitialize)(this.MagicPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MagicPic;
+        private System.Windows.Forms.TextBox Question;
+        private System.Windows.Forms.Label Answer;
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.Label labelK;
+    }
+}
+
---
+++ Magic Ball/Magic Ball/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic_Ball
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random x = new Random();
+        private void MagicPic_Click(object sender, EventArgs e)
+        {
+            double p = x.NextDouble();
+            //double p = 0.95;
+            labelP.Text = "p = " + p;
+            int k = -1;
+            double[] P = new double[7] {0.125,0.250,0.375, 0.500, 0.625,0.750, 0.875}; //сами отметки
+            double[] prob = new double[8];
+
+            //вычисляем промежутки
+            prob[0] = P[0];
+            for (int i = 1; i < 7; i++)
+            {
+                prob[i] = P[i] - P[i-1];
+            }
+            prob[7] = 1 - P[6];
+
+            while (!(p <= 0))
+            {
+                k += 1;
+                p -= prob[k];
+            }
+            labelK.Text = "k = " + k;
+            switch (k)
+            {
+                case 0:
+                    Answer.Text = "Подумай ещё раз";
+                    break;
+                case 1:
+                    Answer.Text = "Тебе лучше не знать";
+                    break;
+                case 2:
+                    Answer.Text = "Время покажет";
+                    break;
+                case 3:
+                    Answer.Text = "Нельзя точно сказать";
+                    break;
+                case 4:
+                    Answer.Text = "Скорее да, чем нет";
+                    break;
+                case 5:
+                    Answer.Text = "Скорее нет, чем да";
+                    break;
+                case 6:
+                    Answer.Text = "Спроси в другой раз";
+                    break;
+                case 7:
+                    Answer.Text = "Это твоя судьба";
+                    break;
+            }
+            Answer.Visible = true;
+        }
+
+        private void Question_TextChanged(object sender, EventArgs e)
+        {
+            Question.ForeColor = Color.Black;
+        }
+
+        private void Question_Click(object sender, EventArgs e)
+        {
+            Question.Text = "";
+        }
+    }
+}
